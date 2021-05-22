11ab66c04ce3db7d99dcd84ec9d7ca0d1c3bbfd3
SkyShi <62084226+SkyShi-wvb@users.noreply.github.com>
2021-05-22 14:53:32 +0800

    Add files via upload

---
+++ js/src/admin/index.js
@@ -0,0 +1,54 @@
+import app from 'flarum/app';
+
+const EMBED_TYPES = ['preview-with-link', 'preview-without-link', 'full-with-link', 'full-without-link'];
+
+app.initializers.add('imgur-upload', () => {
+    app.extensionData
+        .for('matteocontrini-imgur-upload')
+        .registerSetting(
+            {
+                setting: 'imgur-upload.client-id',
+                label: 'Imgur Client ID',
+                type: 'text'
+            }
+        )
+        .registerSetting(
+            {
+                setting: 'imgur-upload.hide-markdown-image',
+                label: app.translator.trans('imgur-upload.admin.settings.hide-markdown-image'),
+                type: 'boolean'
+            }
+        )
+        .registerSetting(
+            {
+                setting: 'imgur-upload.allow-paste',
+                label: app.translator.trans('imgur-upload.admin.settings.allow-paste'),
+                type: 'boolean'
+            }
+        )
+        .registerSetting(
+            {
+                setting: 'imgur-upload.embed-type',
+                label: app.translator.trans('imgur-upload.admin.settings.embed-type.title'),
+                type: 'select',
+                options: Object.fromEntries(
+                    EMBED_TYPES.map(x => [x, app.translator.trans(`imgur-upload.admin.settings.embed-type.${x}`)])
+                ),
+                default: 'preview-with-link'
+            }
+        )
+        .registerSetting(
+            {
+                setting: 'imgur-upload.admin.settings.API-URL',
+                label: app.translator.trans('imgur-upload.admin.settings.API-URL'),
+                type: 'text','https://api.imgur.com/v3/upload'
+            }
+        )
+        .registerSetting(
+            {
+                setting: 'imgur-upload.admin.settings.image-URL',
+                label: app.translator.trans('imgur-upload.image-URL'),
+                type: 'text','https://i.imgur.com'
+            }   
+        )
+});
